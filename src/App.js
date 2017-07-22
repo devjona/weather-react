247f87084d98d7583bf34cf54aed3bf3bae9c869
Jonathan Alorda <jonathan.alorda@gmail.com>
2017-07-21 20:13:17 -0500

    Search input is controlled component but callback to parent component isn't happening until user presses ENTER. I might change this in the future but I wanted the practice to only call the API on ENTER key press; I can always go back to lodash.

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
                     <code>src/App.js</code>
                     and save to reload. You did it!
                 </p>
+                {/* <SearchBar /> */}
                 <SearchBar onCitySearch={this.getCityList} />
             </div>
         );
@@ -35,12 +36,27 @@ class App extends Component {
     // api will return an array of possible cities
     getCityList(city) {
         let weather = new XMLHttpRequest();
-        weather.open('GET', `http://api.wunderground.com/api/e65ca2760713be4f/conditions/q/${city}.json`);
+        weather.open('GET',
+            `http://api.wunderground.com/api/e65ca2760713be4f/conditions/q/${city}.json`,
+            true
+        );
+
+        console.log('weather 1: ', weather);
+
+        weather.onload = function(e) {
+            if (weather.readyState === 4) {
+                if (weather.status === 200) {
+                    console.log('weather 2: ', weather);
+                    // typeof weather is object. typeof weather.response is string; parse it!
+                    let responseParsed = JSON.parse(weather.response);
+                    console.log('responseParsed.res: ', responseParsed.response.results);
+                }
+            }
+
+        };
         weather.send(null);
-        // this should return an array of all the possible cities and states that match the user's typing.
-        let response = JSON.parse(weather.response.results)
-        });
     }
+
 
     // from list of cities, user should pick which 'Austin', e.g., Austin, TX, so we make another request with a slightly different URL that includes state and city
     // you'll need another one to take the click the user selects:
@@ -59,7 +75,7 @@ class App extends Component {
         weather.open('GET', `http://api.wunderground.com/api/e65ca2760713be4f/forecast/q/${state}/${city}.json`);
         weather.send(null);
 
-        let response = JSON.parse(weather.current_observation) // bunch of weather data for the present
+        let response = JSON.parse(weather.current_observation) // not sure if I need to change the weather observation to somethiong else
     }
 
 }
--- src/searchBar.js
+++ src/searchBar.js
@@ -4,7 +4,10 @@ class SearchBar extends Component {
     constructor(props) {
         super(props)
 
-        this.state = { city: ''};
+        this.state = {
+            city: '',
+            cityEntered: ''
+        };
     }
 
     render() {
@@ -12,16 +15,28 @@ class SearchBar extends Component {
             <div className="searchBar">
                 <input
                     placeholder="Enter a city to get the weather forecast!"
-                    value={this.state.city}
                     onChange={event => this.onInputChange(event.target.value)}
+                    value={this.state.city}
+                    // event handler for when 'enter' is pressed
+                    onKeyDown = {event => this.onEnterKey(event, event.target.value)}
                 />
             </div>
-        )
+        );
     }
 
+    // this is only letting user input change state to change the value of the input
     onInputChange(city) {
+        console.log('city: ', city);
         this.setState({city});
-        this.props.onCitySearch(city);
+    }
+
+    onEnterKey(event, city) {
+        if (event.keyCode === 13) {
+            console.log('event: ', event);
+            console.log('city: ', city);
+            this.setState({cityEntered: city});
+            this.props.onCitySearch(city);
+        }
     }
 }
 
