b0e99f649486a0b88326d59c188cb65bd9bdfcbe
Jonathan Alorda <jonathan.alorda@gmail.com>
2018-09-01 15:34:45 -0500

    I\'m using fetch succesfully but it is currently commented out as I\'m trying to only write the apiCall and parse function one as (what I think is) a Class Method, however, I still have more to learn about ES6.

--- src/App.js
+++ src/App.js
@@ -21,19 +21,18 @@ class App extends Component {
         console.log('constructor this: ', this);
         this.getCityList = this.getCityList.bind(this);
         this.getCityWeather = this.getCityWeather.bind(this);
+        // this.apiCall = this.apiCall.bind(this);
         // this.getCityForecast = this.getCityForecast.bind(this);
     }
 
-    
+    // I only want to write this function once and call it inside of others but I'm having a scope issue...
+    apiCall(api) {
+        fetch(api)
+            .then(res => res.json());
+    }
     // gets user input into the search bar, send that city to the api
     // api will return an array of possible cities
     getCityList(city) {
-        // debugger;
-        let parsedRequest;
-        let citiesList;
-        // const api = `http://api.wunderground.com/api/e65ca2760713be4f/conditions/q/${city}.json`;
-
-        // We are going to replace this call with fetch()
         /* 
         1 Call the weather API
         2 Parse the response
@@ -41,65 +40,83 @@ class App extends Component {
         4 Based upon that, you can setState with either the results or the error message
         */
 
-        // fetch(api)
-        //     .then(response => response.json())
-        //     .then(parsedResponse => {
-        //         console.log(`res: ${parsedResponse.response}`);
-        //         console.log(`this: ${this}`);
-                
-                
-        //         if (parsedResponse.results) {
-        //                 citiesList = parsedRequest.response.results;
-        //                 this.setState({citiesList})
-        //         } else if (parsedResponse.response.error) {
+        // let parsedResponse;
+        console.log(`this is: ${this}`);
+        
+        let citiesList;
+        const api = `http://api.wunderground.com/api/e65ca2760713be4f/conditions/q/${city}.json`;
+
+        // How can I extract this function a level up so I don't have to rewrite it for the next API call?
+
+        const parsedResponse = (api) =>
+            this.apiCall(api)
+                .then(console.log)
+
+        parsedResponse(api);        
+
+        // This is working :)
+        // const parsedResponse = () => 
+        //     fetch(api)
+        //         .then(response => response.json());
+
+        // parsedResponse()
+        //     // .then(console.log)
+        //     .then(response => {
+        //         if (response.response.results) {
+        //             this.setState({
+        //                 citiesList: response.response.results
+        //             })
+        //         } else if (response.response.error) {
         //             console.log(`in else if`);
-                    
-        //             return parsedResponse.response.error.description
+        //             this.setState({
+        //                 cityNotFound: response.response.error.description
+        //             })
+        //             return response.response.error.description
         //         }
         //     })
+    }
 
         // I need to handle the response. Should I have a helper function I pass to a .then()?
 
         // Forget almost everything below this line except for when you are setting state
 
-        let request = new XMLHttpRequest();
-        request.open('GET',
-        `http://api.wunderground.com/api/e65ca2760713be4f/conditions/q/${city}.json`,
-        true
-        );
-        // Because it's async, request.onload() doesn't fire until request.send() completes.
-        // onload = what you want to do with the request once you receive a response
-        request.onload = () => {
-            console.log('request onload!');
-            console.log(`request!: ${request}`);
+        // let request = new XMLHttpRequest();
+        // request.open('GET',
+        // `http://api.wunderground.com/api/e65ca2760713be4f/conditions/q/${city}.json`,
+        // true
+        // );
+        // // Because it's async, request.onload() doesn't fire until request.send() completes.
+        // // onload = what you want to do with the request once you receive a response
+        // request.onload = () => {
+        //     console.log('request onload!');
+        //     console.log(`request!: ${request}`);
             
-            if (request.response) {
+        //     if (request.response) {
 
-                parsedRequest = JSON.parse(request.response);
-                console.log(`parsedRequest: ${parsedRequest}`);
+        //         parsedRequest = JSON.parse(request.response);
+        //         console.log(`parsedRequest: ${parsedRequest}`);
                 
-            }
-            // this is really ugly error handling but this is what comes before using promises and fetch...
-            if (parsedRequest.response.results) {
+        //     }
+        //     // this is really ugly error handling but this is what comes before using promises and fetch...
+        //     if (parsedRequest.response.results) {
 
-                citiesList = parsedRequest.response.results;
-                console.log('cl1: ', citiesList);
-                this.setState({citiesList});
-            } else {
-                console.log(`no results, only errors. State doesn't change. Page doesn't refresh.`);
-                this.setState({
-                    cityNotFound: parsedRequest.response.error.description
-                })
+        //         citiesList = parsedRequest.response.results;
+        //         console.log('cl1: ', citiesList);
+        //         this.setState({citiesList});
+        //     } else {
+        //         console.log(`no results, only errors. State doesn't change. Page doesn't refresh.`);
+        //         this.setState({
+        //             cityNotFound: parsedRequest.response.error.description
+        //         })
                 
             
-            }
-        }
+        //     }
+        // }
         
-        request.send();
-        // console.log('cL2: ', citiesList);
-        console.log('this gCL: ', this);
-    }
-    
+    //     request.send();
+    //     // console.log('cL2: ', citiesList);
+    //     console.log('this gCL: ', this);
+    // }
     
     // from list of cities, user should pick which 'Austin', e.g., Austin, TX, so we make another request with a slightly different URL that includes state and city
     // you'll need another one to take the click the user selects:
